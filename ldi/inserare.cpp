7730be03e1877bedd65995a54879d2f5485817ba
xVllad44v2 <xvlad44@gmail.com>
2022-05-18 22:05:17 +0300

    da

--- ldi/inserare.cpp
+++ ldi/inserare.cpp
@@ -24,6 +24,21 @@ void AdaugareFinal(nod * & prim , nod * & ultim, int x){
     }
 
 }
+void InserareDupaQ(nod *& prim,nod *& ultim, nod * q,int x){
+    nod *nou = new nod;
+    nou->info=x;
+    nou->urm=NULL;
+    nou->ant=NULL;
+    
+    if(prim==NULL || ultim==NULL){
+        prim=ultim=NULL;
+    }else{
+        nou->urm = q->urm;
+        q->urm = nou;
+        nou->ant=q;
+    }
+}
+
 void AdaugareInceput(nod * & prim , nod * & ultim, int x){
     nod *nou = new nod;
     nou->info=x;
@@ -41,33 +56,16 @@ void AdaugareInceput(nod * & prim , nod * & ultim, int x){
     }
 }
 void InserareInainteQ(nod *& prim,nod *& ultim, nod * q,int x){
-    nod* nou = new nod;
-    nou -> info = x;
-    if (q == prim || prim == NULL)
-    {
-        AdaugareInceput(prim,ultim,x);
-    }else{
-    nod *p=new nod;
-
-    for(p=prim;p->urm!=q;p=p->urm);
-
-    nou -> urm = p->urm;
-    nou->ant=p;
-    p->urm=nou;
-    }
-
-}
-void InserareDupaQ(nod *& prim,nod *& ultim, nod * q,int x){
     nod *nou = new nod;
-    nou->info=x;
-    nou->urm=NULL;
-    nou->ant=NULL;
-    
-    if(prim==NULL || ultim==NULL){
-        AdaugareFinal(prim,ultim,x);
-    }else{
-        nou->urm = q->urm;
-        q->urm = nou;
-        nou->ant=q;
+    nou->info = x;
+    if(q==prim || prim == NULL)
+        AdaugareInceput(prim,ultim,x);
+    else{
+        nod *p = new nod;
+        for(p=prim;p->urm!=q;p=p->urm);
+        nou->urm=q;
+        q->ant=nou;
+        nou->ant=p;
+        p->urm=nou;
     }
 }
